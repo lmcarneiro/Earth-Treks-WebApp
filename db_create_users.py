f17a406d129a9b75c26b422bd11b940de0c5c3e1
Lucas Carneiro <36357748+lmcarneiro@users.noreply.github.com>
2021-09-24 11:15:34 -0400

    Update db_create_users.py

--- db_create_users.py
+++ db_create_users.py
@@ -6,8 +6,6 @@ from project.models import User
 
 #insert data
 #db.session.add(User("lucas", "lmcarneiro@smcm.edu", "Krave9!i7"))
-#db.session.add(User("evelina", "Ev.cebotari@gmail.com", "MyLumps7*"))
-#db.session.add(User("kat", "katherine.e.lamb@gmail.com", "Borbluv<3"))
 
 #commit the changes
 db.session.commit()
